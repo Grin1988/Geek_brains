797a0bcb610eb7edac172270f425b1c8a6535e84
Evgeniy Sogrin <sogrin_es@mail.ru>
2023-02-08 20:08:04 +0300

    08/02/2023

---
+++ HomeWork3/Task_23/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает 
+//на вход пятизначноен число и проверяет,   
+//является ли оно палиндромом.
+
+//14212-> нет
+//23432-> да
+//12821-> да
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = InputNumber("Введите пятизначное число: ");
+
+
+
---
+++ Seminar_3/Task_22/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает 
+//на вход число (N) и выдаёт таблицу квадратов 
+//чисел от 1 до N.
+
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+
+int InputNumber(string str)
+{
+    System.Console.WriteLine(str);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int N = InputNumber("Введите число N: ");
+
+// int num = 1;
+// while(num <= N)
+// {
+//     System.Console.Write(Math.Pow(num, 2) + ",");
+//     num++;
+// }
+
+for(int num = 1; num <= N; num++)
+{
+    System.Console.Write(Math.Pow(num, 2) + ",");
+} 
