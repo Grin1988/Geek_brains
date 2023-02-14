76ea14c7022d65b846bcb775e672d46c80af5b75
Evgeniy Sogrin <sogrin_es@mail.ru>
2023-02-15 00:33:03 +0300

    Seminar_5

---
+++ Seminar_5/Task_32/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 32: Напишите программу замена элементов 
+// массива: положительные элементы замените на 
+// соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size]; //выделим память по массив
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1); // -9, 9
+    }
+    return array;
+}
+
+void PrinArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+
+void SumNegativePositive(int[] array, out int sumPositive, out int sumNegative) // 3 метод описания (находим сразу и отр и полож числа)
+{
+    sumNegative = 0; // out означает, что мы можем вытаскивать из данного метода значения, неважно произошел return или нет
+    sumPositive = 0;
+
+    int[] result = new int[2];
+    for (int i = 0; i < array.Length; i++) // проходим по всем числам массива
+    {
+        if (array[i] < 0) // Проверяем условие на их отрицательность
+        {
+            sumNegative += array[i]; // Складываем отрицательньные в 0 ячейку
+        }
+        else
+        {
+            sumPositive += array[i];  // иначе складыаем положительные в 1 ячейку
+        }
+    }
+}
+
+void ChangeNagativeAndPositive(int[] array)
+{
+    for (int i = 0; i < array.Length; i++) // проходим по всем числам массива
+    {
+        if (array[i] < 0) // Проверяем условие на их отрицательность
+        {
+            array[i] = -array[i]; // Меняем местами отрицательное с положительным числом
+        }
+        else
+        {
+            array[i] = -array[i];
+        }
+
+    }
+}
+
+
+int[] myArray = GenerateArray(12, -9, 9);
+PrinArray(myArray);
+SumNegativePositive(myArray, out int sumPositive, out int sumNegative);
+ChangeNagativeAndPositive(myArray);
+PrinArray(myArray);
+// System.Console.WriteLine($"Сумма отрицательных элементов равна {sumNegative}");
+// System.Console.WriteLine($"Сумма положительных элементов равна {sumPositive}");
+
+
---
+++ Seminar_5/Task_33/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая 
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, -3] -> да
+
+int GetNumber(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    int[] array = new int[size]; //выделим память по массив
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(leftRange, rightRange + 1); // -9, 9
+    }
+    return array;
+}
+
+void PrinArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+void SearchElement(int[] array, int element)
+{
+    for (int i = 0; i < array.Length; i++) // проходим по всем числам массива
+    {
+        if (element == array[i]) // Проверяем условие на нахождение нашего элемента
+        {
+            System.Console.Write("Такое значение есть5"); ; // Складываем отрицательньные в 0 ячейку
+        }
+        else
+        {
+            System.Console.Write("Нет такого значения");  // 
+        }
+    }
+}
+
+int elementsear = GetNumber("Введите искомое число: ");
+int[] myArray = GenerateArray(20, -9, 9);
+PrinArray(myArray);
+SearchElement(myArray, elementsear);
+
