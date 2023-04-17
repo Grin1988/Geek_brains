5626894160ba4dde8de3be57db20144948912cca
Evgeniy Sogrin <sogrin_es@mail.ru>
2023-04-17 23:24:22 +0300

    Home_work_3

---
+++ Stepic/2.py
@@ -0,0 +1,15 @@
+# print('b', 'a', 'c', 'k')
+# print('end')
+
+# print('b', 'a', 'c', 'k', sep="")
+# print('end', sep=' ')
+
+# print('b', 'a', 'c', 'k', end="!")
+# print('end', end="")
+
+# print('b', 'a', 'c', 'k', sep="!", end=' ')
+# print('end', 'end', sep='-')
+# print('python' 'for'' backend')
+
+print('hello', end='\n\n')
+print('world')
